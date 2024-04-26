f701807b7102ea2a276c6574f2d1c1bc4f36ff1a
andre <andre.melendez5835@gmail.com>
2024-04-26 16:56:39 -0600

    Updated GUI

---
+++ Lab_6/Lab_6_main.java
@@ -0,0 +1,3 @@
+public class Lab_6_main {
+    
+}
---
+++ Lab_6/gui/sidePanel.java
@@ -0,0 +1,8 @@
+import javax.swing.*; // imports classes and components of the Java API that provides components for building graphical user interface (GUI)
+import java.awt.*; // imports classes, interfaces, and enumerations from Abstract Window Toolkit (AWT), used for building windows, buttons, text fields, etc.
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class sidePanel {
+    
+}
