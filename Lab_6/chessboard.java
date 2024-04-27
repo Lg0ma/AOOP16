72abd3f1e08c1cf379da2d971484eb68942f007f
andre <andre.melendez5835@gmail.com>
2024-04-26 18:51:29 -0600

    Deleted files, added ActionListeners and actionPerforned functions

--- Lab_6/chessboard.java
+++ Lab_6/chessboard.java
@@ -1,8 +1,20 @@
 import javax.swing.*; // imports classes and components of the Java API that provides components for building graphical user interface (GUI)
+
+import enums.chess_piece_type;
+
 import java.awt.*; // imports classes, interfaces, and enumerations from Abstract Window Toolkit (AWT), used for building windows, buttons, text fields, etc.
+import java.awt.event.ActionEvent; // imports classes for outputs from user inputs
+import java.awt.event.ActionListener; // imports classes to get user inputs
 
 // class for creating the chessboard window with its operations
 public class chessboard extends JFrame {
+    // initialize variables to hold the user inputs
+    private static String selectedPieceType = "";
+    private static String selectedPieceColor = "";
+    private static String selectedPieceRow = "";
+    private static String selectedPieceCol = "";
+    private static String newPieceRow = "";
+    private static String newPieceCol = "";
 
     // a container
     static JPanel panel = new JPanel(); // a generic, lightweight container, wil be added to the overarching container to be displayed
@@ -13,12 +25,11 @@ public class chessboard extends JFrame {
     Tile[][] boardCells;
     // a button side panel
     JPanel buttonPanel;
-
     // the chessboard function
     public chessboard(){
         // sets the title of the panel
         super();
-        setTitle("Chess Board");
+        setTitle("Chess Game (Lab 6)");
         // sets the panel with a bordered layout
         panel.setLayout(new BorderLayout()); // BorderLayout organizes the panel to have NORTH EAST SOUTH WEST sides
         // initialize a panel for the chessboard and its grid layout
@@ -61,7 +72,7 @@ public class chessboard extends JFrame {
         }
     }
 
-// function to set the components for the sidePanel
+// class to set the components for the sidePanel
 public static JPanel sidePanel(){   
     // initialize the grey color
     Color gray = new Color(192,192,192);
@@ -98,14 +109,121 @@ public static JPanel sidePanel(){
     // alignments for dropdowns and buttons
     colDropdown.setMaximumSize(colDropdown.getPreferredSize());
     colDropdown.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+    // add action listener to the colDropdown menu
+    colDropdown.addActionListener(new ActionListener() {
+        @Override
+        // output from action
+        public void actionPerformed(ActionEvent e) {
+            // get the dropdown menu
+            JComboBox<String> comboBox = (JComboBox<String>) e.getSource();
+            // get the currently selected item in the dropdown menu
+            selectedPieceCol = (String) comboBox.getSelectedItem();
+            // display the selected color
+            JFrame popUpFrame = new JFrame("Selected Initial Column");
+            JLabel messageLabel = new JLabel("You selected the column: " + selectedPieceCol);
+            messageLabel.setHorizontalAlignment(SwingConstants.CENTER);
+            popUpFrame.add(messageLabel);
+            popUpFrame.setSize(300, 100);
+            popUpFrame.setLocationRelativeTo(null);
+            popUpFrame.setVisible(true);
+        }
+    });
+
     rowDropdown.setMaximumSize(rowDropdown.getPreferredSize());
     rowDropdown.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+    // add action listener to the rowDropdown menu
+    rowDropdown.addActionListener(new ActionListener() {
+        @Override
+        // output from action
+        public void actionPerformed(ActionEvent e) {
+            // get the dropdown menu
+            JComboBox<String> comboBox = (JComboBox<String>) e.getSource();
+            // get the currenly selected item in the dropdown menu
+            selectedPieceRow = (String) comboBox.getSelectedItem();
+            // display the selected color
+            JFrame popUpFrame = new JFrame("Selected Initial Row");
+            JLabel messageLabel = new JLabel("You selected the row: " + selectedPieceRow);
+            messageLabel.setHorizontalAlignment(SwingConstants.CENTER);
+            popUpFrame.add(messageLabel);
+            popUpFrame.setSize(300, 100);
+            popUpFrame.setLocationRelativeTo(null);
+            popUpFrame.setVisible(true);
+        }
+    });
+
     colorDropdown.setAlignmentX(Component.CENTER_ALIGNMENT);
     colorDropdown.setMaximumSize(colorDropdown.getPreferredSize());
+
+    // add action listener to the colorDropdown menu
+    colorDropdown.addActionListener(new ActionListener() {
+        @Override
+        // output from action
+        public void actionPerformed(ActionEvent e) {
+            // get the dropdown menu
+            JComboBox<String> comboBox = (JComboBox<String>) e.getSource();
+            // get the currently selected item in the dropdown menu
+            selectedPieceColor = (String) comboBox.getSelectedItem();
+            // display the selected color
+            JFrame popUpFrame = new JFrame("Selected Color");
+            JLabel messageLabel = new JLabel("You selected the color: " + selectedPieceColor);
+            messageLabel.setHorizontalAlignment(SwingConstants.CENTER);
+            popUpFrame.add(messageLabel);
+            popUpFrame.setSize(300, 100);
+            popUpFrame.setLocationRelativeTo(null);
+            popUpFrame.setVisible(true);
+        }
+    });
+
+
+
     finalColDropdown.setAlignmentX(Component.CENTER_ALIGNMENT);
     finalColDropdown.setMaximumSize(finalColDropdown.getPreferredSize());
+
+    // add action listener to the colDropdown menu
+    finalColDropdown.addActionListener(new ActionListener() {
+        @Override
+        // output from action
+        public void actionPerformed(ActionEvent e) {
+            // get the dropdown menu
+            JComboBox<String> comboBox = (JComboBox<String>) e.getSource();
+            // get the currently selected item in the dropdown menu
+            newPieceCol = (String) comboBox.getSelectedItem();
+            // display the selected color
+            JFrame popUpFrame = new JFrame("Selected New Column");
+            JLabel messageLabel = new JLabel("You selected the column: " + newPieceCol);
+            messageLabel.setHorizontalAlignment(SwingConstants.CENTER);
+            popUpFrame.add(messageLabel);
+            popUpFrame.setSize(300, 100);
+            popUpFrame.setLocationRelativeTo(null);
+            popUpFrame.setVisible(true);
+        }
+    });
+
     finalRowDropdown.setAlignmentX(Component.CENTER_ALIGNMENT);
     finalRowDropdown.setMaximumSize(finalRowDropdown.getPreferredSize());
+
+        // add action listener to the rowDropdown menu
+        finalRowDropdown.addActionListener(new ActionListener() {
+            @Override
+            // output from action
+            public void actionPerformed(ActionEvent e) {
+                // get the dropdown menu
+                JComboBox<String> comboBox = (JComboBox<String>) e.getSource();
+                // get the currenly selected item in the dropdown menu
+                newPieceRow = (String) comboBox.getSelectedItem();
+                // display the selected color
+                JFrame popUpFrame = new JFrame("Selected New Row");
+                JLabel messageLabel = new JLabel("You selected the row: " + newPieceRow);
+                messageLabel.setHorizontalAlignment(SwingConstants.CENTER);
+                popUpFrame.add(messageLabel);
+                popUpFrame.setSize(300, 100);
+                popUpFrame.setLocationRelativeTo(null);
+                popUpFrame.setVisible(true);
+            }
+        });
+
     // other buttons
     JButton createButton = new JButton("Create piece");
     JButton moveButton = new JButton("Move piece");
@@ -117,8 +235,25 @@ public static JPanel sidePanel(){
         buttonType.setAlignmentX(Component.CENTER_ALIGNMENT);
         buttonPieces.add(buttonType);
         buttonsPanel.add(buttonType);
-        // Removing all space around button panel so they look closer
-    }
+
+        // add action listener to the chessPieces buttons
+        buttonType.addActionListener(new ActionListener() {
+            @Override
+            // output from action
+            public void actionPerformed(ActionEvent e) {
+                // display the selected type
+                JFrame popUpFrame = new JFrame("Selected Chess Piece Type");
+                JLabel messageLabel = new JLabel("You selected the chess piece: " + type);
+                // set the universal var for type
+                selectedPieceType = type;
+                messageLabel.setHorizontalAlignment(SwingConstants.CENTER);
+                popUpFrame.add(messageLabel);
+                popUpFrame.setSize(300, 100);
+                popUpFrame.setLocationRelativeTo(null);
+                popUpFrame.setVisible(true);
+            }
+        });
+        }
     // buttons alignment
     createButton.setAlignmentX(Component.CENTER_ALIGNMENT);
     moveButton.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -137,6 +272,7 @@ public static JPanel sidePanel(){
     initialPosPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
     initialPosPanel.setLayout(new BoxLayout(initialPosPanel, BoxLayout.X_AXIS));
     initialPosPanel.add(colDropdown);
+
     initialPosPanel.add(rowDropdown);
     buttonsPanel.add(initialPosPanel);
     buttonsPanel.add(Box.createVerticalStrut(20));
@@ -152,8 +288,42 @@ public static JPanel sidePanel(){
     buttonsPanel.add(Box.createVerticalStrut(40));
     // add create and move buttons
     buttonsPanel.add(createButton);
+
+    // qdd action listener to the "Create Piece" button
+    createButton.addActionListener(new ActionListener() {
+        @Override
+        // output from action
+        public void actionPerformed(ActionEvent e) {
+            // display the selected variables
+            JFrame popUpFrame = new JFrame("Created a Chess Piece");
+            JLabel messageLabel = new JLabel("You created the following chess piece: " + selectedPieceType + " " + selectedPieceColor + " " + selectedPieceCol + " " + selectedPieceRow);
+            messageLabel.setHorizontalAlignment(SwingConstants.CENTER);
+            popUpFrame.add(messageLabel);
+            popUpFrame.setSize(600, 600);
+            popUpFrame.setLocationRelativeTo(null);
+            popUpFrame.setVisible(true);
+        }
+    });
+
     buttonsPanel.add(Box.createVerticalStrut(5)); // add space between buttons
     buttonsPanel.add(moveButton);
+
+    // qdd action listener to the "Create Piece" button
+    moveButton.addActionListener(new ActionListener() {
+        @Override
+        // output from action
+        public void actionPerformed(ActionEvent e) {
+            // display the selected variables
+            JFrame popUpFrame = new JFrame("Move Chess Piece");
+            JLabel messageLabel = new JLabel("You tried to move " + selectedPieceType + " " + selectedPieceColor + " " + selectedPieceCol + " " + selectedPieceRow + " to: " + newPieceCol + newPieceRow);
+            messageLabel.setHorizontalAlignment(SwingConstants.CENTER);
+            popUpFrame.add(messageLabel);
+            popUpFrame.setSize(600, 600);
+            popUpFrame.setLocationRelativeTo(null);
+            popUpFrame.setVisible(true);
+        }
+    });
+
     // return the panel
     return buttonsPanel;
 }
@@ -173,7 +343,7 @@ class Tile extends JPanel { // extends to JPanel so that we can make each tile a
 
     // method that adjusts the size of the tiles, is overridden
     @Override
-    public Dimension getPreferredSize() { // getPrefferedSize() is a built in function for .swing
+    public Dimension getPreferredSize() { // getPreferredSize() is a built in function for .swing
         // gets the preferred size of the tiles using the super class, is determined by the size of the panel where the tiles will be contained
         Dimension size = super.getPreferredSize();
         // calculates the size for the tiles
@@ -182,5 +352,3 @@ class Tile extends JPanel { // extends to JPanel so that we can make each tile a
         return new Dimension(maxSize, maxSize);
     }
 }
-
-
